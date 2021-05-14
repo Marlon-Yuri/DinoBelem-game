f28ae77e2f8cd30c666255890a5e2d3c93cfad13
Marlon-Yuri <marlonyurimi@gmail.com>
2021-05-14 00:03:08 -0300

    Add details and moves

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-var personagem = document.getElementById("personagem");
-var bloco = document.getElementById("bloco");
+let personagem = document.getElementById("personagem");
+let bloco = document.getElementById("bloco");
+let fundo = document.getElementById("fundo");
 
 function jump(){
     if(personagem.classList != "animate")
     personagem.classList.add("animate")
     setTimeout(function(){
         personagem.classList.remove("animate");
-    }, 400);
+    }, 620);
 }
-var limite = setInterval(function(){
-    var alto =
+let gameOver= setInterval(function(){
+    let personagemTop =
     parseInt(window.getComputedStyle(personagem).
     getPropertyValue("top"));
+    let blocoLeft = 
+    parseInt(window.getComputedStyle(bloco).
+    getPropertyValue("left"));
+    if(blocoLeft <130 && blocoLeft >0 &&
+    personagemTop>=180){
+        bloco.style.display ="none";
+        alert("VOCÊ FOI CORONGADA, ATUALIZE A PÁGINA E PRESSIONE 'OK' PRA CHEGAR NO POSTO DE VACINAÇÃO MAIS PRÓXIMO")
+    }
+    
    
 },10);
